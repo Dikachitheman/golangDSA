c4532e1e49c7111b434935ac2a33f753c491136c
dikachitheman <dikachi.anosike@gmail.com>
2024-01-15 12:50:41 +0100

    leet

--- hashmaps/2/groupPeople.go
+++ hashmaps/2/groupPeople.go
@@ -4,6 +4,40 @@ import (
 	"fmt"
 )
 
+func groupPeople(arr []int) {
+
+	makeMap := make(map[int][]int)
+	count := make(map[int]int)
+	// i := 0
+
+	for _, value := range arr {
+
+		makeMap[value] = make([]int, value)
+		count[value] = 0
+		
+	}
+
+	for index, value := range arr {
+
+		makeMap[value][count[value]] = index
+		count[value]++
+
+		fmt.Println(makeMap[value])
+	}
+
+	fmt.Println(makeMap)
+
+	// return
+}
+
 func main() {
 	
+	// groupSizes := []int{3,3,3,3,3,1,3}
+	// output := groupPeople(groupSizes)
+
+	// groupSizes2 := []int{2,1,3,3,3,2}
+	// output2 := groupPeople(groupSizes2)
+
+	groupSizes := []int{3,3,1,3}
+	groupPeople(groupSizes)
 }
