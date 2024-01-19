2bebc0418a5de3a88eca6e85ba72c90c5ef87372
dikachitheman <dikachi.anosike@gmail.com>
2024-01-19 11:16:11 +0100

    leet

--- hashmaps/2/2groupPeople.go
+++ hashmaps/2/2groupPeople.go
@@ -34,29 +34,57 @@ func groupPeople(arr []int) {
 
 	fmt.Println(makeMap)
 
+	number := 0
+
 	for index, value := range makeMap {
+
+		number = (len(value) / index) + number
+
+	}
+
+	i := 0
+	arrayArray := make([][]int, number)
+
+	for index, value := range makeMap {
+
+		indexA := 0
+		indexB := index
 
 		if len(value) == index {
 
-			arrayArray[index] = value
+			arrayArray[i] = value
+			i++
 
 		} else {
 
-			
+			for x := 0; x < len(value) / index; x++ {
+
+				// fmt.Println("a", indexA)
+				// fmt.Println("b", indexB)
+				// fmt.Println("i", index)
+
+				arrayArray[i] = value[indexA:indexB]
+				indexA += index
+				indexB = indexB + index
+				i++
+
+			}
 
 		}
 
 	}
 
+	fmt.Println(arrayArray)
+
 }
 
 func main () {
-	groupSizes := []int{3,3,3,3,3,1,3}
+	groupSizes := []int{3,3,3,3,3,1,3,2,2,3,3,3,2,2}
 	groupPeople(groupSizes)
 
 	groupSizes2 := []int{2,1,3,3,3,2}
 	groupPeople(groupSizes2)
 
-	// groupSizes := []int{3,3,1,3}
-	// groupPeople(groupSizes)
+	groupSizes3 := []int{3,3,1,3}
+	groupPeople(groupSizes3)
 }
